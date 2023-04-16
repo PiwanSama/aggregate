f22ae056d582e55d33cb41bd494cc9142beb3e23
Piwan Samalie <piwan.summerlee@gmail.com>
2023-04-16 22:45:31 +0300

    Restored navigation component

--- resources/js/components/TheNavigationComponent.vue
+++ resources/js/components/TheNavigationComponent.vue
@@ -1,40 +1,38 @@
 <template>
-  <header class="p-2">
-    <div class="container">
-      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-        <router-link to = "/" class="navbar-brand text-dark d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">Aggregate</router-link>
-        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-          <li class="nav-item">
+        <nav class="navbar navbar-expand-lg">
+  <div class="container-fluid">
+    <router-link to = "/" class="navbar-brand text-dark">Aggregate</router-link>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
             <router-link :to = "{
               name : 'ALevelPoints'
             }" class="nav-item nav-link">My Grades</router-link>
-          </li>
-          <li class="nav-item">
-              <router-link :to = "{
-                name : 'Universities'
-              }" class="nav-item nav-link">Explore Universities</router-link>
-          </li>
-          <li class="nav-item">
+        </li>
+        <li class="nav-item">
             <router-link :to = "{
-                name : 'Login'
-              }" class="nav-item nav-link">My Account</router-link>
-          </li>
-        </ul>
-
-        <div class="text-end">
-          <button type="button" class="btn btn-outline-light me-2">Login</button>
-          <button type="button" class="btn btn-warning">Sign-up</button>
-        </div>
-      </div>
+              name : 'Universities'
+            }" class="nav-item nav-link">Explore Universities</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link :to = "{
+              name : 'Login'
+            }" class="nav-item nav-link">My Account</router-link>
+        </li>
+      </ul>
     </div>
-  </header>
+  </div>
+</nav>
 </template>
 
 <style scoped>
 
-header{
+.navbar{
     background: #fff !important;
-    box-shadow: 0 2px 2px rgba(63,92,110,.15);
+    box-shadow: 0 2px 2px rgba(63,92,110,.15);;
 }
 
 .navbar-brand {
@@ -58,22 +56,21 @@ header{
 }
 
 
-.nav-link.active::before, .nav-link:hover::before, .nav-link:focus::before {
+.navbar-nav .nav-link.active::before, .nav-link:hover::before, .nav-link:focus::before {
     transform-origin: left top;
     transform: scale(1, 1);
 }
 
 
-.nav .nav-link {
+.nav-link {
     position: relative;
     text-decoration: none;
     transition: color 0.1s,background-color 0.1s,padding 0.2s ease-in;
     font-size: 15px !important;
     margin: 5px;
-    color: #247BA0 !important;
 }
 
-.nav .nav-link.active, .nav-link:hover, .nav-link:focus{
+.navbar-nav .nav-link.active, .nav-link:hover, .nav-link:focus{
     color: #247BA0 !important;
     font-weight: 600;
     text-decoration: none;
